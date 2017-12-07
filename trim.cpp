b8cb2ca83d9061fca36a4dc977192c58d8650c4e
Gabriel <31956986+Kadukashi@users.noreply.github.com>
2017-12-07 13:37:47 +0100

    FILE UPLOADED!

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,35 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+#include "trim.h"
+
+ void trim(const char* source, char* trimmed_string)
+ {
+    int len = strlen(source);
+    char opiString[STRLEN];
+    char newString[STRLEN];
+    int countForwards = 0;
+    int countBackwards = len;
+
+    while(source[countForwards] == ' ') {
+        countForwards++;
+    }
+    while(source[countBackwards] == ' ') {
+        countBackwards--;
+    }
+    if (len == 0) {
+      strcpy(trimmed_string, source);
+    }
+    else
+    {
+      int len1 = len - countBackwards;
+      for (int i = 0; i < len2; i++) {
+        opiString[i] = source[i];
+      }
+      for (int i = 0; i < len; i++) {
+        newString[i] = opiString[i];
+      }
+      strcpy(trimmed_string, newString);
+    }
+
+ }
