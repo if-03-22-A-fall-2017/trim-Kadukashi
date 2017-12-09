f1391bdfe4485f43716260915b15928a434084c3
Gabriel <31956986+Kadukashi@users.noreply.github.com>
2017-12-09 19:08:28 +0100

    Add files via upload

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,71 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+#include "trim.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+void 	trim (const char *source, char *trimmed_string)
+{
+  strcpy(trimmed_string, source);
+  int len = strlen(trimmed_string);
+  int count = 0;
+  bool IsTrimmed = false;
+  int newLen = 0;
+
+  front(trimmed_string,len,count,newLen,IsTrimmed);
+
+  IsTrimmed = false;
+  count = 0;
+  newLen = 0;
+  len = strlen(trimmed_string);
+  
+  back(trimmed_string,len,count,newLen,IsTrimmed);
+
+}
+void front(char *trimmed_string, int len, int count, int newLen, bool IsTrimmed)
+{
+  for(int i = 0; i < len && IsTrimmed == false; i++)
+  {
+    if (trimmed_string[i] == ' ')
+    {
+      count++;
+    }
+    else
+    {
+      IsTrimmed = true;
+    }
+  }
+  newLen = len - count;
+  char *TrimmedFront = new char[newLen];
+  int posTrimmedString = count;
+  for(int i = 0; i < newLen; i++)
+  {
+    TrimmedFront[i] = trimmed_string[posTrimmedString];
+    posTrimmedString++;
+  }
+  strcpy(trimmed_string, TrimmedFront);
+}
+void back(char *trimmed_string, int len, int count, int newLen, bool IsTrimmed)
+{
+  for(int i = len-1; i < len && IsTrimmed == false; i--)
+  {
+    if (trimmed_string[i] == ' ')
+    {
+      count++;
+    }
+    else
+    {
+      IsTrimmed = true;
+    }
+  }
+  newLen = len - count;
+  char *TrimmedBack = new char[newLen];
+  for(int i = 0; i < newLen; i++)
+  {
+    TrimmedBack[i] = trimmed_string[i];
+  }
+  strcpy(trimmed_string, TrimmedBack);
+}
--- trim.h
+++ trim.h
@@ -10,3 +10,11 @@
  * Interface for trim
  * ----------------------------------------------------------
  */
+
+ #ifndef ___TRIM_H
+ #define  ___TRIM_H
+ #define STRLEN 16
+  void 	trim (const char *source, char *trimmed_string);
+  void front(char *trimmed_string, int len, int count, int newLen, bool IsTrimmed);
+  void back(char *trimmed_string, int len, int count, int newLen, bool IsTrimmed);
+ #endif
